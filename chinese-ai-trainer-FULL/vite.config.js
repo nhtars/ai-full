ec530f86129634338fec79cf688fbaffa9a8b34f
nhtars <thanhnh2011@gmail.com>
2025-10-20 18:08:39 +0700

    Update vite.config.js

--- chinese-ai-trainer-FULL/vite.config.js
+++ chinese-ai-trainer-FULL/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  build: { outDir: "dist" }
+  base: './', // ⚠️ rất quan trọng cho Vercel
+  build: {
+    outDir: 'dist'
+  }
 })
